ae5c0fa0097051382720997d551c8ddc95b0332b
Sakae Shalom Church <100064296+sakaeshalom@users.noreply.github.com>
2022-04-06 12:06:32 +0900

    script: Message if make failed

--- script/cron-friday.sh
+++ script/cron-friday.sh
@@ -11,8 +11,14 @@ fi
 
 cd $d || exit 1
 
+function msg_failure
+{
+	( echo Failed to burn DVD; cat "$@" | grep -v '\r' | tail ) |
+	~/script/dd.py --channel 'automation' --send-text -
+}
+
 make step0 &> step0.out || true
 make -k step1 &> step1.out || true
-make -k step2 &> step2.out
+make -k step2 &> step2.out || msg_failure step2.out
 
 rm -f obs-${d}-cut.wav obs-${d}-audio-edit.wav
