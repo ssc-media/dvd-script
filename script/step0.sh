3570dddf3230c8c32f87ca82d6fea7d541e56446
Sakae Shalom Church <100064296+sakaeshalom@users.noreply.github.com>
2024-07-27 18:15:07 +0900

    script: Limit the video length
    Friday: 19:30 - 21:30, where usually ends at around 20:45
    Sunday: 10:25 - 12:55, where usually ends at around 12:00

--- script/step0.sh
+++ script/step0.sh
@@ -2,16 +2,19 @@
 
 date=$(basename $(pwd))
 obs_recording_ss=0
+obs_recording_to=4:00:00
 case $(LANG=C date -d $date +%a) in
 	Fri)
 		recording=$(ls -S ~/Videos/obs-${date}-19*.{flv,mkv} | head -n 1)
 		obs_recording_ss=45
+		obs_recording_to=2:00:00
 		run_dvd=n
 		dvd_sources="dvdvideo-${date}.mpg"
 		;;
 	Sun)
 		recording=$(ls -S ~/Videos/obs-${date}-10*.{flv,mkv} | head -n 1)
 		obs_recording_ss=15
+		obs_recording_to=2:30:00
 		run_dvd=y
 		ymd_fri="$(date -d "$date - 2days" +%Y%m%d)"
 		dvd_sources="$(ls ../${ymd_fri}/dvdvideo-${ymd_fri}.mpg || :) dvdvideo-${date}.mpg"
@@ -35,7 +38,7 @@ date=$date
 # inputs
 obs_recording_fmt=$obs_recording_fmt
 obs_recording=$recording
-obs_recording_inp_opt=-ss ${obs_recording_ss}
+obs_recording_inp_opt=-ss ${obs_recording_ss} -to ${obs_recording_to}
 dvd_sources=${dvd_sources}
 
 # outputs
